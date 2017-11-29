54c04857569c4a56d7017ac6b7587b65607f679f
zhou <chuwuwang@126.com>
2017-11-29 16:12:35 +0800

    添加学习笔记。

---
+++ Java/GenericArray.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Array;
+
+public class GenericArray<T> {
+
+	private T[] array;
+
+	@SuppressWarnings("unchecked")
+	public GenericArray(Class<T> type, int size) {
+		array = (T[]) Array.newInstance(type, size);
+	}
+
+	public void put(int index, T item) {
+		array[index] = item;
+	}
+
+	public T get(int index) {
+		return array[index];
+	}
+
+	public T[] create() {
+		return array;
+	}
+
+}
