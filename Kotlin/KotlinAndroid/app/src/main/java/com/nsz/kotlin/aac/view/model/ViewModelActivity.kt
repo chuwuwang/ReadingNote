4969f66bb3019b2c00aa053fe57217331dee30b1
zhou <nishenzhou@sunmi.com>
2020-05-15 01:06:29 +0800

    添加ViewModel

---
+++ Kotlin/KotlinAndroid/app/src/main/java/com/nsz/kotlin/aac/view/model/ViewModelActivity.kt
@@ -0,0 +1,17 @@
+package com.nsz.kotlin.aac.view.model
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ViewModelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+
+    }
+
+}
